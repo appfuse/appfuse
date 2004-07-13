842c83f95a35215e0299bcd952467e4546ccf7f1
Matt Raible <matt@raibledesigns.com>
2004-07-13 05:04:21 +0000

    Changed StartupListener to extend Spring's ContextLoaderListener.  This solves the duplicate loading problem that causes Quartz issues.

--- src/web/org/appfuse/webapp/listener/StartupListener.java
+++ src/web/org/appfuse/webapp/listener/StartupListener.java
@@ -1,19 +1,18 @@
 package org.appfuse.webapp.listener;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.appfuse.Constants;
 import org.appfuse.service.LookupManager;
-import org.springframework.web.context.ContextLoader;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.context.ContextLoaderListener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -21,26 +20,28 @@ import org.springframework.web.context.support.XmlWebApplicationContext;
  * and populate any application-wide drop-downs.
  *
  * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
- * @version $Revision: 1.7 $ $Date: 2004/05/16 02:17:03 $
+ * @version $Revision: 1.8 $ $Date: 2004/07/13 05:04:21 $
  *
  * @web.listener
  */
-public class StartupListener implements ServletContextListener {
+public class StartupListener extends ContextLoaderListener
+        implements ServletContextListener {
     private static Log log = LogFactory.getLog(StartupListener.class);
-    private ServletContext context;
 
-    public void contextInitialized(ServletContextEvent sce) {
-        context = sce.getServletContext();
-
+    public void contextInitialized(ServletContextEvent event) {
         if (log.isDebugEnabled()) {
-            log.debug("contextInitialized...");
+            log.debug("initializing context...");
         }
+        // call Spring's context ContextLoaderListener to initialize
+        // all the context files specified in web.xml
+        super.contextInitialized(event);
 
+        ServletContext context = event.getServletContext();
         String daoType = context.getInitParameter(Constants.DAO_TYPE);
 
         // if daoType is not specified, use DAO as default
         if (daoType == null) {
-            log.warn("No 'daoType' Context Parameter supplied in web.xml, using default (hibernate)");
+            log.warn("No 'daoType' context carameter, using hibernate");
             daoType = Constants.DAO_TYPE_HIBERNATE;
         }
 
@@ -63,50 +64,25 @@ public class StartupListener implements ServletContextListener {
         }
 
         setupContext(context);
-        
-        // output the retrieved values for the Init and Context Parameters
-        if (log.isDebugEnabled()) {
-            log.debug("Initialization complete [OK]");
-        }
     }
 
-    public void contextDestroyed(ServletContextEvent sce) {
-        if (log.isDebugEnabled()) {
-            log.debug("contextDestroyed...");
-        }
-
-        context = null;
-    }
-    
     public static void setupContext(ServletContext context) {
         // populate drop-downs and stuff in servlet context
         try {
             // check to see if ctx has already been set
-            XmlWebApplicationContext ctx =
-                (XmlWebApplicationContext) context
+            ApplicationContext ctx =
+                (ApplicationContext) context
                     .getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
-            if (ctx == null) {
-                // if the context is null, it's likely because the listeners
-                // aren't initialized in the same order they're specified in
-                // web.xml.  This happens in Tomcat 5.
-                ctx = new XmlWebApplicationContext();
-                // get the config locations from context parameters
-                String configLocations =
-                        context.getInitParameter(ContextLoader.CONFIG_LOCATION_PARAM);
-                String[] files = configLocations.split(",");
-                for (int i=0; i < files.length; i++) {
-                    files[i] = files[i].trim();
-                }
-                ctx.setConfigLocations(files);
-                ctx.setServletContext(context);
-                ctx.refresh();
-            }
-            
+
             LookupManager mgr =
                 (LookupManager) ctx.getBean("lookupManager");
 
             // get list of possible roles
             context.setAttribute(Constants.AVAILABLE_ROLES, mgr.getAllRoles());
+
+            if (log.isDebugEnabled()) {
+                log.debug("drop-down initialization complete [OK]");
+            }
         } catch (Exception e) {
             log.error("Error populating drop-downs failed!" + e.getMessage());
             e.printStackTrace();
