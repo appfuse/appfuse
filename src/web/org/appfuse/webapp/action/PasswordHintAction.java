9bb2a885c7f7a524c83eaefb78d62fc6bf64a647
Matt Raible <matt@raibledesigns.com>
2004-08-15 06:07:20 +0000

    Issue number:  45 Converted RegistrationServlet to be StrutsActions

---
+++ src/web/org/appfuse/webapp/action/PasswordHintAction.java
@@ -0,0 +1,92 @@
+package org.appfuse.webapp.action;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.action.ActionMessages;
+import org.apache.struts.util.MessageResources;
+import org.apache.struts.util.RequestUtils;
+
+import org.appfuse.Constants;
+import org.appfuse.model.User;
+import org.appfuse.service.MailSender;
+import org.appfuse.service.UserManager;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Action class to send password hints to registered users.
+ *
+ * <p>
+ * <a href="PasswordHintAction.java.html"><i>View Source</i></a>
+ * </p>
+ *
+ * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
+ *
+ * @struts.action path="/passwordHint" validate="false"
+ * @struts.action-forward name="previousPage" path="/login.jsp"
+ */
+public final class PasswordHintAction extends BaseAction {
+    
+    private static Log log = LogFactory.getLog(PasswordHintAction.class);
+
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response)
+    throws Exception {
+        MessageResources resources = getResources(request);
+        ActionMessages errors = new ActionMessages();
+        String userId = request.getParameter("username");
+
+        // ensure that the username has been sent
+        if (userId == null) {
+            log.warn("Username not specified, notifying user that it's a required field.");
+
+            errors.add(ActionMessages.GLOBAL_MESSAGE,
+                       new ActionMessage("errors.required",
+                                         resources.getMessage("userFormEx.username")));
+            saveErrors(request, errors);
+            return mapping.findForward("previousPage");
+        }
+
+        if (log.isDebugEnabled()) {
+            log.debug("Processing Password Hint...");
+        }
+
+        ActionMessages messages = new ActionMessages();
+
+        // look up the user's information
+        try {
+            UserManager userMgr = (UserManager) getBean("userManager");
+
+            User user = userMgr.getUser(userId);
+
+            StringBuffer msg = new StringBuffer();
+            msg.append("Your password hint is: " + user.getPasswordHint());
+            msg.append("\n\nLogin at: " + RequestUtils.serverURL(request) +
+                       request.getContextPath());
+
+            // From,to,cc,subject,content
+            MailSender.sendTextMessage(Constants.DEFAULT_FROM,
+                                       user.getEmail(), null,
+                                       "Password Hint", msg.toString());
+            messages.add(ActionMessages.GLOBAL_MESSAGE,
+                         new ActionMessage("login.passwordHint.sent", userId,
+                                           user.getEmail()));
+            saveMessages(request.getSession(), messages);
+        } catch (Exception e) {
+            // If exception is expected do not rethrow
+            errors.add(ActionMessages.GLOBAL_MESSAGE,
+                       new ActionMessage("login.passwordHint.error", userId));
+            saveErrors(request, errors);
+        }
+
+        return mapping.findForward("previousPage");
+    }
+}
---
+++ src/web/org/appfuse/webapp/action/SignupAction.java
@@ -0,0 +1,163 @@
+package org.appfuse.webapp.action;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.apache.struts.Globals;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.action.ActionMessages;
+import org.apache.struts.util.MessageResources;
+import org.apache.struts.util.RequestUtils;
+
+import org.appfuse.Constants;
+import org.appfuse.model.User;
+import org.appfuse.service.MailSender;
+import org.appfuse.service.UserManager;
+import org.appfuse.util.StringUtil;
+import org.appfuse.webapp.form.UserForm;
+import org.appfuse.webapp.util.RequestUtil;
+
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Action class to allow users to self-register.
+ * <p/>
+ * <p/>
+ * <a href="SignupAction.java.html"><i>View Source</i></a>
+ * </p>
+ *
+ * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
+ * @struts.action name="userFormEx" path="/signup" scope="request"
+ *  validate="true" input="failure"
+ * @struts.action-forward name="failure" path=".signup"
+ * @struts.action-forward name="success" path="/mainMenu.html" redirect="true"
+ */
+public final class SignupAction extends BaseAction {
+
+    private static Log log = LogFactory.getLog(SignupAction.class);
+
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response)
+            throws Exception {
+        if (log.isDebugEnabled()) {
+            log.debug("registering user...");
+        }
+
+        if (isCancelled(request)) {
+            return new ActionForward("/");
+        }
+
+        ActionMessages errors = new ActionMessages();
+        UserForm userForm = (UserForm) form;
+        User user = (User) convert(form);
+        String algorithm =
+                (String) getConfiguration().get(Constants.ENC_ALGORITHM);
+
+        // Set the default user role on this new user
+        user.addRole(Constants.USER_ROLE);
+
+        try {
+
+            if (algorithm == null) { // should only happen for test case
+                log.debug("assuming testcase, setting algorigthm to 'SHA'");
+                algorithm = "SHA";
+            }
+
+            user.setPassword(StringUtil.encodePassword(user.getPassword(),
+                    algorithm));
+            UserManager mgr = (UserManager) getBean("userManager");
+            user = mgr.saveUser(user);
+
+            // Set cookies for auto-magical login ;-)
+            String loginCookie = mgr.createLoginCookie(user.getUsername());
+            RequestUtil.setCookie(response, Constants.LOGIN_COOKIE,
+                    loginCookie, request.getContextPath());
+        } catch (Exception e) {
+            if ((e.getMessage() != null) &&
+                    (e.getMessage().indexOf("Duplicate entry") != -1)) {
+                // user already exists!
+                log.warn("User already exists: " + e.getMessage());
+                errors.add(ActionMessages.GLOBAL_MESSAGE,
+                        new ActionMessage("errors.existing.user",
+                                userForm.getUsername(),
+                                userForm.getEmail()));
+                saveErrors(request, errors);
+                return mapping.getInputForward();
+            }
+
+            e.printStackTrace();
+            errors.add(ActionMessages.GLOBAL_MESSAGE,
+                    new ActionMessage("errors.general"));
+
+            while (e != null) {
+                // catch duplicate entry exceptions
+                String msg = e.getMessage();
+
+                if ((msg.indexOf("Duplicate entry") != -1) ||
+                        (msg.indexOf("could not insert") != -1)) {
+                    log.warn("User already exists: " + e.getMessage());
+                    errors.add(ActionMessages.GLOBAL_MESSAGE,
+                            new ActionMessage("errors.existing.user",
+                                    userForm.getUsername(),
+                                    userForm.getEmail()));
+
+                    break;
+                }
+
+                errors.add(ActionMessages.GLOBAL_MESSAGE,
+                        new ActionMessage("errors.detail", e.getMessage()));
+
+                e = (Exception) e.getCause();
+            }
+
+            saveErrors(request, errors);
+            return mapping.getInputForward();
+        }
+
+        ActionMessages messages = new ActionMessages();
+        MessageResources resources = getResources(request);
+
+        messages.add(ActionMessages.GLOBAL_MESSAGE,
+                new ActionMessage("user.registered", userForm.getUsername()));
+
+        HttpSession session = request.getSession();
+        session.setAttribute(Globals.MESSAGE_KEY, messages);
+        session.setAttribute(Constants.REGISTERED, Boolean.TRUE);
+
+        // Send user an e-mail
+        if (log.isDebugEnabled()) {
+            log.debug("Sending user '" + userForm.getUsername()
+                    + "' an account information e-mail");
+        }
+
+        StringBuffer msg = new StringBuffer();
+        msg.append(resources.getMessage("signup.email.message"));
+        msg.append("\n\n" + resources.getMessage("userFormEx.username"));
+        msg.append(": " + userForm.getUsername() + "\n");
+        msg.append(resources.getMessage("userFormEx.password") + ": ");
+        msg.append(userForm.getPassword());
+        msg.append("\n\nLogin at: " + RequestUtils.serverURL(request) +
+                request.getContextPath());
+
+        String subject = resources.getMessage("signup.email.subject");
+
+        try {
+            // From,to,cc,subject,content
+            MailSender.sendTextMessage(Constants.DEFAULT_FROM,
+                    userForm.getEmail(), null,
+                    subject, msg.toString());
+        } catch (MessagingException me) {
+            log.warn("Failed to send Account Information e-mail");
+        }
+
+        return mapping.findForward("success");
+    }
+
+}
