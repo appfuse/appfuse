1530629001d27b1946d8036b14849eeecd1ff718
Matt Raible <matt@raibledesigns.com>
2004-10-09 20:43:54 +0000

    no message

---
+++ release.sh
@@ -0,0 +1,18 @@
+ant dist
+cd ..
+rm -rf appfuse-spring-dist
+mkdir appfuse-spring-dist
+cp -r appfuse appfuse-spring-dist/.
+cd appfuse-spring-dist/appfuse
+ant install-springmvc
+rm -r extras/spring
+ant dist
+
+cd ../..
+rm -rf appfuse-webwork-dist
+mkdir appfuse-webwork-dist
+cp -r appfuse appfuse-webwork-dist/.
+cd appfuse-webwork-dist/appfuse
+ant install-webwork 
+rm -rf extras/webwork
+ant dist
