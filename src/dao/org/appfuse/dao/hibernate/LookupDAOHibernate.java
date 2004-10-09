896fcfa763cb542a079f38567859f0f3019ccc3f
Matt Raible <matt@raibledesigns.com>
2004-10-09 07:15:18 +0000

    Cleaned up javadocs and simplified HQL

--- src/dao/org/appfuse/dao/hibernate/LookupDAOHibernate.java
+++ src/dao/org/appfuse/dao/hibernate/LookupDAOHibernate.java
@@ -8,9 +8,7 @@ import org.appfuse.dao.LookupDAO;
 /**
  * Hibernate implementation of LookupDAO.
  *
- * <p>
- * <a href="LookupDAOHibernate.java.html"><i>View Source</i></a>
- * </p>
+ * <p><a href="LookupDAOHibernate.java.html"><i>View Source</i></a></p>
  *
  * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
  */
@@ -24,6 +22,6 @@ public class LookupDAOHibernate extends BaseDAOHibernate implements LookupDAO {
             log.debug("retrieving all role names...");
         }
 
-        return getHibernateTemplate().find("from Role r order by r.name");
+        return getHibernateTemplate().find("from Role order by name");
     }
 }
