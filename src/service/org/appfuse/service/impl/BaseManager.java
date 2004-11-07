79de06e3c82a4537269218c909405703ba38b14a
Matt Raible <matt@raibledesigns.com>
2004-11-07 23:03:08 +0000

    Added methods to talk to do generic CRUD

--- src/service/org/appfuse/service/impl/BaseManager.java
+++ src/service/org/appfuse/service/impl/BaseManager.java
@@ -1,15 +1,57 @@
 package org.appfuse.service.impl;
 
+import java.io.Serializable;
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.appfuse.dao.DAO;
+import org.appfuse.service.Manager;
 
 /**
- * Base class for Business Services - use this class for utility methods.
+ * Base class for Business Services - use this class for utility methods and
+ * generic CRUD methods.
  * 
  * <p><a href="BaseManager.java.html"><i>View Source</i></a></p>
  *
  * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
  */
-public class BaseManager {
+public class BaseManager implements Manager {
     protected final Log log = LogFactory.getLog(getClass());
+    protected DAO dao = null;
+    
+    /**
+     * @see org.appfuse.service.Manager#setDAO(org.appfuse.dao.DAO)
+     */
+    public void setDAO(DAO dao) {
+        this.dao = dao;
+    }
+    
+    /**
+     * @see org.appfuse.service.Manager#getObject(java.lang.Class, java.io.Serializable)
+     */
+    public Object getObject(Class clazz, Serializable id) {
+        return dao.getObject(clazz, id);
+    }
+    
+    /**
+     * @see org.appfuse.service.Manager#getObjects(java.lang.Class)
+     */
+    public List getObjects(Class clazz) {
+        return dao.getObjects(clazz);
+    }
+    
+    /**
+     * @see org.appfuse.service.Manager#removeObject(java.lang.Class, java.io.Serializable)
+     */
+    public void removeObject(Class clazz, Serializable id) {
+        dao.removeObject(clazz, id);
+    }
+    
+    /**
+     * @see org.appfuse.service.Manager#saveObject(java.lang.Object)
+     */
+    public void saveObject(Object o) {
+        dao.saveObject(o);
+    }
 }
