77ab74b50325069a543dc0ba2d67e8526cd3399f
Matt Raible <matt@raibledesigns.com>
2004-11-07 23:03:47 +0000

    Generic CRUD interface and configuration

--- src/dao/org/appfuse/dao/hibernate/BaseDAOHibernate.java
+++ src/dao/org/appfuse/dao/hibernate/BaseDAOHibernate.java
@@ -1,65 +1,57 @@
 package org.appfuse.dao.hibernate;
 
-import java.lang.reflect.Method;
+import java.io.Serializable;
+import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 import org.appfuse.dao.DAO;
 import org.springframework.orm.ObjectRetrievalFailureException;
-
 import org.springframework.orm.hibernate.support.HibernateDaoSupport;
 
 /**
  * This class serves as the Base class for all other DAOs - namely to hold
- * common methods that they might all use.</p>
+ * common methods that they might all use. Can be used for standard CRUD
+ * operations.</p>
  *
  * <p><a href="BaseDAOHibernate.java.html"><i>View Source</i></a></p>
  *
  * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
  */
 public class BaseDAOHibernate extends HibernateDaoSupport implements DAO {
-    protected transient final Log log = LogFactory.getLog(getClass());
-
-    /**
-     * @see org.appfuse.dao.DAO#getObject(java.lang.Object)
-     */
-    public Object getObject(Object o) {
-        Long id = null;
-
-        // use reflection to get the id and fetch the object
-        try {
-            Method meth = o.getClass().getMethod("getId", new Class[0]);
-            id = (Long) meth.invoke(o, new Object[0]);
-            o = getHibernateTemplate().get(o.getClass(), id);
-        } catch (Exception e) {
-            throw new ObjectRetrievalFailureException(o.getClass(), id);
-        }
-
-        return o;
-    }
+    protected final Log log = LogFactory.getLog(getClass());
 
     /**
      * @see org.appfuse.dao.DAO#saveObject(java.lang.Object)
      */
-    public Object saveObject(Object o) {
-        getHibernateTemplate().saveOrUpdateCopy(o);
+    public void saveObject(Object o) {
+        getHibernateTemplate().saveOrUpdate(o);
+    }
+
+    /**
+     * @see org.appfuse.dao.DAO#getObject(java.lang.Class, java.io.Serializable)
+     */
+    public Object getObject(Class clazz, Serializable id) {
+        Object o = getHibernateTemplate().get(clazz, id);
+
+        if (o == null) {
+            throw new ObjectRetrievalFailureException(clazz, id);
+        }
 
         return o;
     }
 
     /**
-     * @see org.appfuse.dao.DAO#removeObject(java.lang.Object)
+     * @see org.appfuse.dao.DAO#getObjects(java.lang.Class)
      */
-    public void removeObject(Object o) {
-        if (log.isDebugEnabled()) {
-            log.debug("loading object to delete....");
-        }
+    public List getObjects(Class clazz) {
+        return getHibernateTemplate().loadAll(clazz);
+    }
 
-        o = getObject(o);
-
-        if (o != null) {
-            getHibernateTemplate().delete(o);
-        }
+    /**
+     * @see org.appfuse.dao.DAO#removeObject(java.lang.Class, java.io.Serializable)
+     */
+    public void removeObject(Class clazz, Serializable id) {
+        getHibernateTemplate().delete(getObject(clazz, id));
     }
 }
