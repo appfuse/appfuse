61a6c10367024747163108863738dfa8dbdbc6a1
Matt Raible <matt@raibledesigns.com>
2004-11-07 23:04:00 +0000

    Generic CRUD interface

--- src/dao/org/appfuse/dao/DAO.java
+++ src/dao/org/appfuse/dao/DAO.java
@@ -1,34 +1,49 @@
 package org.appfuse.dao;
 
+import java.io.Serializable;
+import java.util.List;
+
 
 /**
  * Data Access Object (DAO) interface.   This is an interface
  * used to tag our DAO classes and to provide common methods to all DAOs.
  *
- * <p>
- * <a href="DAO.java.html"><i>View Source</i></a>
- * </p>
+ * <p><a href="DAO.java.html"><i>View Source</i></a></p>
  *
  * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
  */
 public interface DAO {
 
     /**
-     * Generic method to get an object - id must be populated
-     * @param o
-     * @return a populated object (or null if id doesn't exist)
+     * Generic method used to get all objects of a particular type. This
+     * is the same as lookup up all rows in a table.
+     * @param clazz the type of objects (a.k.a. while table) to get data from
+     * @return List of populated objects
      */
-    public Object getObject(Object o);
+    public List getObjects(Class clazz);
+    
+    /**
+     * Generic method to get an object based on class and identifier. An 
+     * ObjectRetrievalFailureException Runtime Exception is thrown if 
+     * nothing is found.
+     * 
+     * @param clazz model class to lookup
+     * @param id the identifier (primary key) of the class
+     * @return a populated object
+     * @see org.springframework.orm.ObjectRetrievalFailureException
+     */
+    public Object getObject(Class clazz, Serializable id);
 
     /**
      * Generic method to save an object - handles both update and insert.
      * @param o the object to save
      */
-    public Object saveObject(Object o);
+    public void saveObject(Object o);
 
     /**
-     * Generic method to delete an object
-     * @param o the object to delete
+     * Generic method to delete an object based on class and id
+     * @param clazz model class to lookup
+     * @param id the identifier (primary key) of the class
      */
-    public void removeObject(Object o);
+    public void removeObject(Class clazz, Serializable id);
 }
