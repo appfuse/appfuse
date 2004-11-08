d69a4c298b0cbb9a9956d1f48126a49547baab23
Matt Raible <matt@raibledesigns.com>
2004-11-08 01:39:21 +0000

    Override generic CRUD Managers (not implemented with iBATIS)

---
+++ extras/ibatis/src/service/org/appfuse/service/Manager.java
@@ -0,0 +1,4 @@
+package org.appfuse.service;
+
+public interface Manager {
+}
---
+++ extras/ibatis/src/service/org/appfuse/service/impl/BaseManager.java
@@ -0,0 +1,21 @@
+package org.appfuse.service.impl;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.appfuse.dao.DAO;
+import org.appfuse.service.Manager;
+
+/**
+ * Base class for Business Services - use this class for utility methods and
+ * generic CRUD methods.
+ *
+ * <p><a href="BaseManager.java.html"><i>View Source</i></a></p>
+ *
+ * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
+ */
+public class BaseManager implements Manager {
+    protected final Log log = LogFactory.getLog(getClass());
+}
