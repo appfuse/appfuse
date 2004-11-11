0c473b4c7cb754e5344f3f2d720fe74d27bc7e3f
Matt Raible <matt@raibledesigns.com>
2004-11-11 18:28:14 +0000

    Updated release configuration to create proper filenames

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ cp -r appfuse appfuse-spring-dist/.
 cd appfuse-spring-dist/appfuse
 ant install-springmvc
 rm -r extras/spring
-ant dist
+ant dist -Darchive.prefix=appfuse-springmvc
 
 cd ../..
 rm -rf appfuse-webwork-dist
@@ -15,4 +15,4 @@ cp -r appfuse appfuse-webwork-dist/.
 cd appfuse-webwork-dist/appfuse
 ant install-webwork 
 rm -rf extras/webwork
-ant dist
+ant dist -Darchive.prefix=appfuse-webwork
