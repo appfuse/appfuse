5452326b9fa20c9e27260844da67bb7b79bf2985
Matt Raible <matt@raibledesigns.com>
2004-12-07 12:23:54 +0000

    Fixed test so it would actually work

--- test/service/org/appfuse/service/UserExistsExceptionTest.java
+++ test/service/org/appfuse/service/UserExistsExceptionTest.java
@@ -1,20 +1,40 @@
 package org.appfuse.service;
 
+import java.util.ResourceBundle;
+
 import org.appfuse.model.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.orm.ObjectRetrievalFailureException;
 
 
 public class UserExistsExceptionTest extends BaseManagerTestCase {
+    private static ApplicationContext ctx = null;
     private UserManager manager = null;
-    private ApplicationContext ctx = null;
-
-    public void testOne() {}
+    private ResourceBundle rb;
     
-    protected void setUp() throws Exception {
-        super.setUp();
-        ctx = new ClassPathXmlApplicationContext("/WEB-INF/applicationContext-*.xml");
+    // This static block ensures that Spring's BeanFactory is only loaded
+    // once for all tests
+    static {
+        // the dao.type is written to the database.properties file
+        // in properties.xml
+        ResourceBundle db = ResourceBundle.getBundle("database");
+        String daoType = db.getString("dao.type");
+        String[] paths = {"/WEB-INF/applicationContext-resources.xml",
+                          "/WEB-INF/applicationContext-" + daoType + ".xml",
+                          "/WEB-INF/applicationContext-service.xml"};
+        ctx = new ClassPathXmlApplicationContext(paths);
+    }
+    
+    public void setUp() throws Exception {
         manager = (UserManager) ctx.getBean("userManager");
+
+        // delete 'foo' in case it exists
+        try {
+            manager.removeUser("foo");
+        } catch (ObjectRetrievalFailureException fe) {
+            // ignore
+        }
     }
 
     public void testAddExistingUser() throws Exception {
