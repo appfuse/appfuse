a2593fa9f54ed668d11abc95ff35d987056b2a09
Matt Raible <matt@raibledesigns.com>
2005-03-11 08:50:45 +0000

    Upgraded MySQL Driver, PostgreSQL Driver, Spring and Hibernate

---
+++ test-all.sh
@@ -0,0 +1,13 @@
+ant setup test-all
+cd extras/appgen
+ant test-all
+cd ../ibatis
+ant test-all
+cd ../jsf
+ant test-all
+cd ../spring
+ant test-all
+cd ../tapestry
+ant test-all
+cd ../webwork
+ant test-all
