3e28272a930e7bb854d08f4d4169edad97215a61
Matt Raible <matt@raibledesigns.com>
2005-08-02 06:12:50 +0000

    Removed unused imports

--- src/dao/org/appfuse/model/UserCookie.java
+++ src/dao/org/appfuse/model/UserCookie.java
@@ -2,9 +2,6 @@ package org.appfuse.model;
 
 import java.util.Date;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
-
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 /**
