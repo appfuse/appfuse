e19c53145064de7178113170c3cca0cf61cc5160
Matt Raible <matt@raibledesigns.com>
2006-01-09 12:24:04 +0000

    Added grant to test@localhost.localdomain which can happen on some Linux distros (http://issues.appfuse.org/browse/APF-36).

--- metadata/sql/mysql-create.sql
+++ metadata/sql/mysql-create.sql
@@ -2,6 +2,9 @@ create database if not exists appfuse;
 grant all privileges on appfuse.* to test@"%" identified by "test";
 grant all privileges on appfuse.* to test@localhost identified by "test";
 
+-- Extra user/host privilege added to account for Fedora Core default hostname quirk.
+grant all privileges on appfuse.* to test@localhost.localdomain identified by "test";
+
 -- You may have to explicitly define your hostname in order for things
 -- to work correctly.  For example:
 -- grant all privileges on appfuse.* to test@host.domain.com identified by "test";
