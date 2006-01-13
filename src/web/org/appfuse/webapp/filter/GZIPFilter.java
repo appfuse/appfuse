2416c51614764afa4ccaa3a3b01bd1ce993dba22
Matt Raible <matt@raibledesigns.com>
2006-01-13 07:13:39 +0000

    Changed to extend Spring's OncePerRequestFilter

--- src/web/org/appfuse/webapp/filter/GZIPFilter.java
+++ src/web/org/appfuse/webapp/filter/GZIPFilter.java
@@ -2,19 +2,15 @@ package org.appfuse.webapp.filter;
 
 import java.io.IOException;
 
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 import org.displaytag.tags.TableTagParameters;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 /**
  * Filter that compresses output with gzip (assuming that browser supports gzip).
@@ -28,32 +24,28 @@ import org.displaytag.tags.TableTagParameters;
  *
  * @web.filter name="compressionFilter"
  */
-public class GZIPFilter implements Filter {
+public class GZIPFilter extends OncePerRequestFilter {
     private final transient Log log = LogFactory.getLog(GZIPFilter.class);
 
-    public void doFilter(ServletRequest req, ServletResponse res,
-                         FilterChain chain)
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, 
+                                 FilterChain chain)
     throws IOException, ServletException {
-        if (req instanceof HttpServletRequest) {
-            HttpServletRequest request = (HttpServletRequest) req;
-            HttpServletResponse response = (HttpServletResponse) res;
 
-            if (isGZIPSupported(request)) {
-                if (log.isDebugEnabled()) {
-                    log.debug("GZIP supported, compressing response");
-                }
-
-                GZIPResponseWrapper wrappedResponse =
-                    new GZIPResponseWrapper(response);
-
-                chain.doFilter(req, wrappedResponse);
-                wrappedResponse.finishResponse();
-
-                return;
+        if (isGZIPSupported(request)) {
+            if (log.isDebugEnabled()) {
+                log.debug("GZIP supported, compressing response");
             }
 
-            chain.doFilter(req, res);
+            GZIPResponseWrapper wrappedResponse =
+                new GZIPResponseWrapper(response);
+
+            chain.doFilter(request, wrappedResponse);
+            wrappedResponse.finishResponse();
+
+            return;
         }
+
+        chain.doFilter(request, response);
     }
 
     /**
@@ -64,8 +56,7 @@ public class GZIPFilter implements Filter {
     private boolean isGZIPSupported(HttpServletRequest req) {
         
         // disable gzip filter for exporting from displaytag
-        String exporting =
-            req.getParameter(TableTagParameters.PARAMETER_EXPORTING);
+        String exporting = req.getParameter(TableTagParameters.PARAMETER_EXPORTING);
         
         if (exporting != null) {
             if (log.isDebugEnabled()) {
@@ -75,9 +66,8 @@ public class GZIPFilter implements Filter {
         }
 
         String browserEncodings = req.getHeader("accept-encoding");
-        boolean supported =
-            ((browserEncodings != null) &&
-            (browserEncodings.indexOf("gzip") != -1));
+        boolean supported = ((browserEncodings != null) &&
+                             (browserEncodings.indexOf("gzip") != -1));
 
         String userAgent = req.getHeader("user-agent");
 
@@ -90,11 +80,5 @@ public class GZIPFilter implements Filter {
         } else {
             return supported;
         }
-    }
-
-    public void init(FilterConfig filterConfig) {
-    }
-
-    public void destroy() {
     }
 }
