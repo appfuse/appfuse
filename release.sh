12b03e6197be8bb2fb4314a46d9a28108d214f85
Matt Raible <matt@raibledesigns.com>
2006-01-14 19:08:03 +0000

    Distribution should be cleaner

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ rm -rf appfuse-spring-dist
 mkdir appfuse-spring-dist
 cp -r appfuse appfuse-spring-dist/.
 cd appfuse-spring-dist/appfuse
-cd extras/spring;ant install;cd ../..
+cd extras/spring;ant install;cd ../..;rm -r extras/spring
 ant dist -Darchive.prefix=appfuse-springmvc
 
 cd ../..
@@ -13,7 +13,7 @@ rm -rf appfuse-webwork-dist
 mkdir appfuse-webwork-dist
 cp -r appfuse appfuse-webwork-dist/.
 cd appfuse-webwork-dist/appfuse
-cd extras/webwork;ant install;cd ../..
+cd extras/webwork;ant install;cd ../..;;rm -r extras/webwork
 ant dist -Darchive.prefix=appfuse-webwork
 
 cd ../..
@@ -21,7 +21,7 @@ rm -rf appfuse-jsf-dist
 mkdir appfuse-jsf-dist
 cp -r appfuse appfuse-jsf-dist/.
 cd appfuse-jsf-dist/appfuse
-cd extras/jsf;ant install;cd ../..
+cd extras/jsf;ant install;cd ../..;rm -r extras/jsf
 ant dist -Darchive.prefix=appfuse-jsf
 
 cd ../..
@@ -29,5 +29,5 @@ rm -rf appfuse-tapestry-dist
 mkdir appfuse-tapestry-dist
 cp -r appfuse appfuse-tapestry-dist/.
 cd appfuse-tapestry-dist/appfuse
-cd extras/tapestry;ant install;cd ../..
+cd extras/tapestry;ant install;cd ../..;rm -r extras/tapestry
 ant dist -Darchive.prefix=appfuse-tapestry
