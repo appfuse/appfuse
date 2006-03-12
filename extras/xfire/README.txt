5d3cf26823eddf17c15edd9f163e0b0d32aa27e2
Mika Goeckel <mikagoeckel@codehaus.org>
2006-03-12 22:45:51 +0000

    APF-297

---
+++ extras/xfire/README.txt
@@ -0,0 +1,18 @@
+Axis Integration Instructions
+================================================================================
+
+More information on the integration of Axis can be found at:
+
+http://spaces.msn.com/members/chenztw/PersonalSpace.aspx?_c=
+
+To install Axis into Appfuse, you need to navigate to this
+directory from the command line.  Then you can execute any of the following
+targets with Ant. It might not be the most robust installer (it'll probably 
+fail if run twice), but it seems to work good enough.
+
+    	install    - which installs everything necessary to use the Spring native way of integration
+
+Please notice that the lib directory of axis contains only minimal filesets. 
+You are encouraged to get the full release with docs and examples. 
+See the README files in the directory.
+================================================================================
---
+++ extras/xfire/test/service/org/appfuse/service/UserWebServiceTest.java
@@ -0,0 +1,67 @@
+package org.appfuse.service;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.appfuse.dao.UserDAO;
+import org.appfuse.model.User;
+import org.codehaus.xfire.spring.AbstractXFireSpringTest;
+import org.jdom.Document;
+import org.jmock.Mock;
+import org.jmock.core.constraint.IsEqual;
+import org.jmock.core.matcher.InvokeOnceMatcher;
+import org.jmock.core.stub.ReturnStub;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UserWebServiceTest extends AbstractXFireSpringTest {
+
+    protected final Log log = LogFactory.getLog(getClass());
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    public void testGetWsdl() throws Exception {
+        Document doc = getWSDLDocument("UserWebService");
+        printNode(doc);
+        
+        assertValid("//xsd:complexType[@name=\"User\"]", doc);
+        assertValid("//xsd:complexType[@name=\"Role\"]", doc);
+    }
+    
+    public void testGetUser() throws Exception {
+        // Setup testharness 
+        User testData = new User("tomcat");
+        testData.setEnabled(true);
+        Mock userDAO = new Mock(UserDAO.class);
+        
+        // because we can't extend MockObjectTestCase we create new instances for once(), eq() and returnValue()
+        InvokeOnceMatcher once = new InvokeOnceMatcher();
+        IsEqual eq = new IsEqual("tomcat");
+        ReturnStub returnValue = new ReturnStub(testData);
+        userDAO.expects(once).method("getUser").with(eq).will(returnValue);
+        
+        UserManager service = (UserManager) getContext().getBean("userManager");
+        service.setUserDAO((UserDAO)userDAO.proxy());
+        
+        // invoke webservice
+        Document response =
+            invokeService("UserWebService", "/org/appfuse/service/getUser.xml");
+
+        printNode(response);
+        // verify result
+        userDAO.verify();
+        addNamespace("service","http://service.appfuse.org");
+        addNamespace("model","http://model.appfuse.org");
+        assertValid("//service:getUserResponse/service:out[model:username=\"tomcat\"]",response);
+        assertValid("//service:getUserResponse/service:out[model:enabled=\"true\"]",response);
+    }
+
+    protected ApplicationContext createContext() {
+        return new ClassPathXmlApplicationContext(new String[]{
+                "org/appfuse/service/applicationContext-test.xml",
+                "org/appfuse/service/applicationContext-webservice.xml"});
+    }
+    
+
+}
