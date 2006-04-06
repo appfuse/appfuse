af1fe9c4668fb305e64a8f5724a1a97e185d8f2b
Matt Raible <matt@raibledesigns.com>
2006-04-06 22:23:32 +0000

    Made database name in db-create more dynamic (http://issues.appfuse.org/browse/APF-114).

--- metadata/sql/mysql-create.sql
+++ metadata/sql/mysql-create.sql
@@ -1,10 +1,10 @@
-create database if not exists appfuse;
-grant all privileges on appfuse.* to test@"%" identified by "test";
-grant all privileges on appfuse.* to test@localhost identified by "test";
+create database if not exists @dbname@;
+grant all privileges on @dbname@.* to test@"%" identified by "test";
+grant all privileges on @dbname@.* to test@localhost identified by "test";
 
 -- Extra user/host privilege added to account for Fedora Core default hostname quirk.
-grant all privileges on appfuse.* to test@localhost.localdomain identified by "test";
+grant all privileges on @dbname@.* to test@localhost.localdomain identified by "test";
 
 -- You may have to explicitly define your hostname in order for things
 -- to work correctly.  For example:
--- grant all privileges on appfuse.* to test@host.domain.com identified by "test";
+-- grant all privileges on @dbname@.* to test@host.domain.com identified by "test";
