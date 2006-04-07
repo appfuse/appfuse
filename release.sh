43fe54d4e03e466130df3752f988e1223e9a9596
Matt Raible <matt@raibledesigns.com>
2006-04-07 01:22:19 +0000

    Polishing for release (in about 2-3 hours)

--- release.sh
+++ release.sh
@@ -7,8 +7,9 @@ rm -rf appfuse-spring-dist
 mkdir appfuse-spring-dist
 cp -r appfuse appfuse-spring-dist/.
 cd appfuse-spring-dist/appfuse
+rm -r extras/myjavapack
 cd extras/spring;ant install;cd ../..;rm -r extras/spring
-ant dist -Darchive.prefix=appfuse-springmvc
+ant dist -Darchive.prefix=appfuse-springmvc -Dnodocs=true
 cp ../*.zip ../../appfuse/dist/release
 
 cd ../..
@@ -16,8 +17,9 @@ rm -rf appfuse-webwork-dist
 mkdir appfuse-webwork-dist
 cp -r appfuse appfuse-webwork-dist/.
 cd appfuse-webwork-dist/appfuse
+rm -r extras/myjavapack
 cd extras/webwork;ant install;cd ../..;rm -r extras/webwork
-ant dist -Darchive.prefix=appfuse-webwork
+ant dist -Darchive.prefix=appfuse-webwork -Dnodocs=true
 cp ../*.zip ../../appfuse/dist/release
 
 cd ../..
@@ -25,8 +27,9 @@ rm -rf appfuse-jsf-dist
 mkdir appfuse-jsf-dist
 cp -r appfuse appfuse-jsf-dist/.
 cd appfuse-jsf-dist/appfuse
+rm -r extras/myjavapack
 cd extras/jsf;ant install;cd ../..;rm -r extras/jsf
-ant dist -Darchive.prefix=appfuse-jsf
+ant dist -Darchive.prefix=appfuse-jsf -Dnodocs=true
 cp ../*.zip ../../appfuse/dist/release
 
 cd ../..
@@ -34,7 +37,8 @@ rm -rf appfuse-tapestry-dist
 mkdir appfuse-tapestry-dist
 cp -r appfuse appfuse-tapestry-dist/.
 cd appfuse-tapestry-dist/appfuse
+rm -r extras/myjavapack
 cd extras/tapestry;ant install;cd ../..;rm -r extras/tapestry
-ant dist -Darchive.prefix=appfuse-tapestry
+ant dist -Darchive.prefix=appfuse-tapestry -Dnodocs=true
 cp ../*.zip ../../appfuse/dist/release
 
