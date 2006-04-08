02fb57a3f14648ebcfb9fb2ea8870933dcecb234
Matt Raible <matt@raibledesigns.com>
2006-04-08 23:55:58 +0000

    Remove extras that don't apply to distribution

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ rm -rf appfuse-spring-dist
 mkdir appfuse-spring-dist
 cp -r appfuse appfuse-spring-dist/.
 cd appfuse-spring-dist/appfuse
-rm -r extras/myjavapack
+rm -r extras/myjavapack extras/jsf extras/tapestry extras/webwork
 cd extras/spring;ant install;cd ../..;rm -r extras/spring
 ant dist -Darchive.prefix=appfuse-springmvc -Dnodocs=true
 cp ../*.zip ../../appfuse/dist/release
@@ -17,7 +17,7 @@ rm -rf appfuse-webwork-dist
 mkdir appfuse-webwork-dist
 cp -r appfuse appfuse-webwork-dist/.
 cd appfuse-webwork-dist/appfuse
-rm -r extras/myjavapack
+rm -r extras/myjavapack extras/jsf extras/spring extras/tapestry
 cd extras/webwork;ant install;cd ../..;rm -r extras/webwork
 ant dist -Darchive.prefix=appfuse-webwork -Dnodocs=true
 cp ../*.zip ../../appfuse/dist/release
@@ -27,7 +27,7 @@ rm -rf appfuse-jsf-dist
 mkdir appfuse-jsf-dist
 cp -r appfuse appfuse-jsf-dist/.
 cd appfuse-jsf-dist/appfuse
-rm -r extras/myjavapack
+rm -r extras/myjavapack extras/spring extras/tapestry extras/webwork
 cd extras/jsf;ant install;cd ../..;rm -r extras/jsf
 ant dist -Darchive.prefix=appfuse-jsf -Dnodocs=true
 cp ../*.zip ../../appfuse/dist/release
@@ -37,7 +37,7 @@ rm -rf appfuse-tapestry-dist
 mkdir appfuse-tapestry-dist
 cp -r appfuse appfuse-tapestry-dist/.
 cd appfuse-tapestry-dist/appfuse
-rm -r extras/myjavapack
+rm -r extras/myjavapack extras/jsf extras/spring extras/webwork
 cd extras/tapestry;ant install;cd ../..;rm -r extras/tapestry
 ant dist -Darchive.prefix=appfuse-tapestry -Dnodocs=true
 cp ../*.zip ../../appfuse/dist/release
