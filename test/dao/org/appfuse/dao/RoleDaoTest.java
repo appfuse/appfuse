04db98545e73d053e08663e14bc3d159137430dc
Matt Raible <matt@raibledesigns.com>
2006-04-24 19:07:27 +0000

    Commit to database before checking to see if record is there

--- test/dao/org/appfuse/dao/RoleDaoTest.java
+++ test/dao/org/appfuse/dao/RoleDaoTest.java
@@ -4,25 +4,24 @@ import org.appfuse.Constants;
 import org.appfuse.model.Role;
 
 public class RoleDaoTest extends BaseDaoTestCase {
-    private Role role = null;
-    private RoleDao dao = null;
+    private RoleDao dao;
 
     public void setRoleDao(RoleDao dao) {
         this.dao = dao;
     }
 
     public void testGetRoleInvalid() throws Exception {
-        role = dao.getRoleByName("badrolename");
+        Role role = dao.getRoleByName("badrolename");
         assertNull(role);
     }
 
     public void testGetRole() throws Exception {
-        role = dao.getRoleByName(Constants.USER_ROLE);
+        Role role = dao.getRoleByName(Constants.USER_ROLE);
         assertNotNull(role);
     }
 
     public void testUpdateRole() throws Exception {
-        role = dao.getRoleByName("user");
+        Role role = dao.getRoleByName("user");
         log.debug(role);
         role.setDescription("test descr");
 
@@ -31,17 +30,19 @@ public class RoleDaoTest extends BaseDaoTestCase {
     }
 
     public void testAddAndRemoveRole() throws Exception {
-        deleteFromTables(new String[]{"user_role", "role"});
-        
-        role = new Role("testrole");
+        Role role = new Role("testrole");
         role.setDescription("new role descr");
-
         dao.saveRole(role);
-        assertNotNull(role.getName());
+        setComplete(); // change behavior from rollback to commit
+        endTransaction();
+
+        startNewTransaction();
+	    role = dao.getRoleByName("testrole");
+        assertNotNull(role.getDescription());
 
         dao.removeRole("testrole");
-
-        endTransaction();
+        setComplete();
+        endTransaction(); // deletes role from database
 
         role = dao.getRoleByName("testrole");
         assertNull(role);
