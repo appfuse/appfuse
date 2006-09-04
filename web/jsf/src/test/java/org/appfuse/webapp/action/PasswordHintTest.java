aaf73beb279b947d547d09662e84ab32c501e9f0
Matt Raible <matt@raibledesigns.com>
2006-09-04 23:06:00 +0000

    Revert back to Dumbster as this seems to be an Ubuntu issue ATM

--- web/jsf/src/test/java/org/appfuse/webapp/action/PasswordHintTest.java
+++ web/jsf/src/test/java/org/appfuse/webapp/action/PasswordHintTest.java
@@ -1,28 +1,27 @@
 package org.appfuse.webapp.action;
 
-import org.subethamail.wiser.Wiser;
+import com.dumbster.smtp.SimpleSmtpServer;
+
 
 public class PasswordHintTest extends BasePageTestCase {
     private PasswordHint bean;
-    
+
     public void setUp() throws Exception {
-        super.setUp();        
+        super.setUp();
         bean = (PasswordHint) getManagedBean("passwordHint");
     }
-    
+
     public void testExecute() throws Exception {
         bean.setUsername("tomcat");
 
-        Wiser wiser = new Wiser();
-        wiser.setPort(2500);
-        wiser.start();
-        
+        SimpleSmtpServer server = SimpleSmtpServer.start(2525);
+
         assertEquals(bean.execute(), "success");
         assertFalse(bean.hasErrors());
 
         // verify an account information e-mail was sent
-        assertTrue(wiser.getMessages().size() == 1);
-        wiser.stop();
+        server.stop();
+        assertTrue(server.getReceivedEmailSize() == 1);
         
         // verify that success messages are in the request
         assertNotNull(bean.getSession().getAttribute("messages"));
--- web/jsf/src/test/java/org/appfuse/webapp/action/SignupFormTest.java
+++ web/jsf/src/test/java/org/appfuse/webapp/action/SignupFormTest.java
@@ -3,46 +3,45 @@ package org.appfuse.webapp.action;
 import org.appfuse.Constants;
 import org.appfuse.model.Address;
 import org.appfuse.model.User;
-import org.subethamail.wiser.Wiser;
+
+import com.dumbster.smtp.SimpleSmtpServer;
 
 public class SignupFormTest extends BasePageTestCase {
     private SignupForm bean;
-    
+
     public void setUp() throws Exception {
-        super.setUp();        
+        super.setUp();
         bean = (SignupForm) getManagedBean("signupForm");
     }
-    
+
     public void testExecute() throws Exception {
         User user = new User("self-registered");
         user.setPassword("Password1");
         user.setConfirmPassword("Password1");
         user.setFirstName("First");
         user.setLastName("Last");
-        
+
         Address address = new Address();
         address.setCity("Denver");
         address.setProvince("CO");
         address.setCountry("USA");
-        address.setPostalCode("80210");        
+        address.setPostalCode("80210");
         user.setAddress(address);
-        
+
         user.setEmail("self-registered@raibledesigns.com");
         user.setWebsite("http://raibledesigns.com");
         user.setPasswordHint("Password is one with you.");
         bean.setUser(user);
-        
+
         // start SMTP Server
-        Wiser wiser = new Wiser();
-        wiser.setPort(2500);
-        wiser.start();
-        
+        SimpleSmtpServer server = SimpleSmtpServer.start(2525);
+
         assertEquals(bean.save(), "mainMenu");
         assertFalse(bean.hasErrors());
-        
+
         // verify an account information e-mail was sent
-        assertTrue(wiser.getMessages().size() == 1);
-        wiser.stop();
+        server.stop();
+        assertTrue(server.getReceivedEmailSize() == 1);
 
         // verify that success messages are in the session
         assertNotNull(bean.getSession().getAttribute(Constants.REGISTERED));
