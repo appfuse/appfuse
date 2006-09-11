67d62b2a420208bdcbc5c4b6d241634b71c09530
Matt Raible <matt@raibledesigns.com>
2006-09-11 05:47:07 +0000

    Changed to skip loading applicationContext-test.xml

--- service/src/test/java/org/appfuse/service/UserExistsExceptionTest.java
+++ service/src/test/java/org/appfuse/service/UserExistsExceptionTest.java
@@ -13,8 +13,9 @@ public class UserExistsExceptionTest extends AbstractTransactionalDataSourceSpri
     
     protected String[] getConfigLocations() {
         setAutowireMode(AUTOWIRE_BY_NAME);
-        // TODO: Figure out how to load DAOs w/o hard-coding the path to hibernate or ibatis
-        return new String[] {"classpath*:/applicationContext-*.xml", "classpath*:/applicationContext-dao.xml"};
+        return new String[] {"/applicationContext-service.xml",
+                             "/applicationContext-resources.xml",
+                             "classpath*:/applicationContext-dao.xml"};
     }
 
     public void testAddExistingUser() throws Exception {
