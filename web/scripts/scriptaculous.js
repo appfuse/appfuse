bd2f2aec77dc526a49432b0b5e5c24ad86c08fc4
Matt Raible <matt@raibledesigns.com>
2006-10-09 15:19:28 +0000

    APF-447: Backed out upgrade to Script.aculo.us 1.6.4 because doesn't work with latest version of Canoo WebTest

--- web/scripts/scriptaculous.js
+++ web/scripts/scriptaculous.js
@@ -1,5 +1,3 @@
-// script.aculo.us scriptaculous.js v1.6.4, Wed Sep 06 11:30:58 CEST 2006
-
 // Copyright (c) 2005 Thomas Fuchs (http://script.aculo.us, http://mir.aculo.us)
 // 
 // Permission is hereby granted, free of charge, to any person obtaining
@@ -22,7 +20,7 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 var Scriptaculous = {
-  Version: '1.6.4',
+  Version: '1.6',
   require: function(libraryName) {
     // inserting via DOM fails in Safari 2.0, so brute force approach
     document.write('<script type="text/javascript" src="'+libraryName+'"></script>');
