4fb7445a7bd2af13ae08035abc0b9b325d5d2f6a
David Whitehurst <dlwhitehurst@gmail.com>
2006-12-02 02:19:19 +0000

    initial import

---
+++ archetypes/appfuse-archetype-basic/create.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+##############################
+mvn archetype:create -DarchetypeGroupId=org.appfuse -DarchetypeArtifactId=appfuse-archetype-basic -DarchetypeVersion=1.0-SNAPSHOT -DgroupId=org.appfuse -DartifactId=myapp 
