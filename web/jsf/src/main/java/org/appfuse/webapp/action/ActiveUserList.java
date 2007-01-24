fd75b7264be423449830936d4e68c8446492f22b
Matt Raible <matt@raibledesigns.com>
2007-01-24 04:14:58 +0000

    Removed unused imports

--- web/jsf/src/main/java/org/appfuse/webapp/action/ActiveUserList.java
+++ web/jsf/src/main/java/org/appfuse/webapp/action/ActiveUserList.java
@@ -2,8 +2,6 @@ package org.appfuse.webapp.action;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 
