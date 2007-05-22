cb839faf00ab232bcb011d78d67ce7aa51dbed90
Matt Raible <matt@raibledesigns.com>
2007-05-22 20:02:52 +0000

    APF-753: Initial commit of "appfuse-core" archetype

---
+++ archetypes/appfuse-core/src/main/resources/archetype-resources/src/main/java/Core.java
@@ -0,0 +1,7 @@
+package $package;
+
+public class Core {
+    public static String getHello() {
+        return "Hello";
+    }
+}
---
+++ archetypes/appfuse-core/src/main/resources/archetype-resources/src/test/java/CoreTest.java
@@ -0,0 +1,9 @@
+package $package;
+
+import junit.framework.TestCase;
+
+public class CoreTest extends TestCase {
+    public void testGetHello() throws Exception {
+        assertEquals("Hello", Core.getHello());
+    }
+}
