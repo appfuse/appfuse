24a4763d4ac6b91231fc0f943a35282b8be49087
Matt Raible <matt@raibledesigns.com>
2007-05-25 14:57:18 +0000

    Changed version to 2.0-rc1-SNAPSHOT (and 1.0-rc1-SNAPSHOT for maven-warpath-plugin)

--- plugins/appfuse-maven-plugin/src/test/java/org/appfuse/mojo/installer/InstallArtifactsMojoTest.java
+++ plugins/appfuse-maven-plugin/src/test/java/org/appfuse/mojo/installer/InstallArtifactsMojoTest.java
@@ -5,12 +5,12 @@ import org.appfuse.mojo.AbstractAppFuseMojoTestCase;
 public class InstallArtifactsMojoTest extends AbstractAppFuseMojoTestCase {
 
     public void testCreateTestProject() throws Exception {
-        createTestProject("appfuse-basic-struts", "2.0-m5");
+        createTestProject("appfuse-basic-struts", "2.0-rc1-SNAPSHOT");
         assertTrue("can't find test-project's pom.xml", checkExists("target/test-project/pom.xml"));
     }
 
     /*public void testGenerateCore() throws Exception {
-        createTestProject("appfuse-basic-struts", "2.0-m5");
+        createTestProject("appfuse-basic-struts", "2.0-rc1-SNAPSHOT");
         File testPom = new File(getBasedir(), "target/test-project/pom.xml");
 
         MavenEmbedder maven = new MavenEmbedder();
