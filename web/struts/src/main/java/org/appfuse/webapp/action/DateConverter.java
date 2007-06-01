f66ba3e1d9c40f5f9974ca3b6d0e14464e5f7033
Matt Raible <matt@raibledesigns.com>
2007-06-01 10:13:42 +0000

    APF-736: Fixed bug in DateConverter

--- web/struts/src/main/java/org/appfuse/webapp/action/DateConverter.java
+++ web/struts/src/main/java/org/appfuse/webapp/action/DateConverter.java
@@ -9,17 +9,18 @@ import org.appfuse.util.DateUtil;
 import com.opensymphony.xwork2.util.TypeConversionException;
 
 public class DateConverter extends StrutsTypeConverter {
+
     public Object convertFromString(Map ctx, String[] value, Class arg2) {
         if (value[0] == null || value[0].trim().equals("")) {
             return null;
         }
+
         try {
-            DateUtil.convertStringToDate(value[0]);
+            return DateUtil.convertStringToDate(value[0]);
         } catch (ParseException pe) {
             pe.printStackTrace();
             throw new TypeConversionException(pe.getMessage());
         }
-        return null;
     }
 
     public String convertToString(Map ctx, Object data) {
