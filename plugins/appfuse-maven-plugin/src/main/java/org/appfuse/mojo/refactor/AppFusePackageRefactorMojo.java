39686a4a7257bfa11e57488fd510f2cbad8f56f4
David Whitehurst <dlwhitehurst@gmail.com>
2007-08-28 03:21:52 +0000

    APF-767:initial import

---
+++ plugins/appfuse-maven-plugin/src/main/java/org/appfuse/mojo/refactor/AppFusePackageRefactorMojo.java
@@ -0,0 +1,18 @@
+package org.appfuse.mojo.refactor;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * Generates Java classes from set of annotated POJOs
+ *
+ * @author <a href="mailto:david@capehenrytech.com">David L. Whitehurst</a>
+ * @goal refactor-packages
+ */
+public class AppFusePackageRefactorMojo extends AbstractMojo {
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
