cf4c0042d83a9a9e14a403b15d65fb76de8ad9eb
Matt Raible <matt@raibledesigns.com>
2007-09-04 07:28:46 +0000

    Change version to 2.0-SNAPSHOT.

--- plugins/appfuse-maven-plugin/src/test/java/org/appfuse/mojo/installer/InstallArtifactsMojoTest.java
+++ plugins/appfuse-maven-plugin/src/test/java/org/appfuse/mojo/installer/InstallArtifactsMojoTest.java
@@ -5,12 +5,12 @@ import org.appfuse.mojo.AbstractAppFuseMojoTestCase;
 public class InstallArtifactsMojoTest extends AbstractAppFuseMojoTestCase {
 
     public void testCreateTestProject() throws Exception {
-        createTestProject("appfuse-basic-struts", "2.0-rc1");
+        createTestProject("appfuse-basic-struts", "2.0-SNAPSHOT");
         assertTrue("can't find test-project's pom.xml", checkExists("target/test-project/pom.xml"));
     }
 
     /*public void testGenerateCore() throws Exception {
-        createTestProject("appfuse-basic-struts", "2.0-rc1");
+        createTestProject("appfuse-basic-struts", "2.0-SNAPSHOT");
         File testPom = new File(getBasedir(), "target/test-project/pom.xml");
 
         MavenEmbedder maven = new MavenEmbedder();
