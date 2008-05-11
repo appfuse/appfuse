b97a8283b8138b8e112536b76a26e121a8692d82
Matt Raible <matt@raibledesigns.com>
2008-05-11 03:02:53 +0000

    Preparing for next development iteration

--- plugins/appfuse-maven-plugin/src/test/java/org/appfuse/mojo/installer/InstallArtifactsMojoTest.java
+++ plugins/appfuse-maven-plugin/src/test/java/org/appfuse/mojo/installer/InstallArtifactsMojoTest.java
@@ -5,12 +5,12 @@ import org.appfuse.mojo.AbstractAppFuseMojoTestCase;
 public class InstallArtifactsMojoTest extends AbstractAppFuseMojoTestCase {
 
     public void testCreateTestProject() throws Exception {
-        createTestProject("appfuse-basic-struts", "2.0.2");
+        createTestProject("appfuse-basic-struts", "2.1-SNAPSHOT");
         assertTrue("can't find test-project's pom.xml", checkExists("target/test-project/pom.xml"));
     }
 
     /*public void testGenerateCore() throws Exception {
-        createTestProject("appfuse-basic-struts", "2.0.2");
+        createTestProject("appfuse-basic-struts", "2.1-SNAPSHOT");
         File testPom = new File(getBasedir(), "target/test-project/pom.xml");
 
         MavenEmbedder maven = new MavenEmbedder();
