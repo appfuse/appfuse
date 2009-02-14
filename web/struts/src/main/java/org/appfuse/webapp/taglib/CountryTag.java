5e62020bf71b01d7004eca99d4ad810d0b82522f
David Whitehurst <dlwhitehurst@gmail.com>
2009-02-14 21:53:39 +0000

    unused import

--- web/struts/src/main/java/org/appfuse/webapp/taglib/CountryTag.java
+++ web/struts/src/main/java/org/appfuse/webapp/taglib/CountryTag.java
@@ -7,7 +7,7 @@ import java.text.Collator;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Iterator;
+//import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 
