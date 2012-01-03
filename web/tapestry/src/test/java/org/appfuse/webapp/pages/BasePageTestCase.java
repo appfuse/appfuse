56260f44f3d51e8db0c3d20e067cfae8bf78b3e8
Serge Eby <serge.eby@gmail.com>
2012-01-03 17:48:27 +0000

    APF-1292: Ugprade to Tapestry 5.3.1 (Part 1 - basic upgrade / no enhancements) - Updated version in POM files - Deleted CheckboxSelect component and replaced with new core component Checklist in tml - Updated BasePageTestCase with workaround for Spring 3.0.6.RELEASE
    Note: For form submission to properly work with mvn jetty:run, you need to temporarly disable
    conflicting Prototype & Scriptaculous in /web/common/src/main/webapp/decorators/default.jsp
    This will be addressed in Part 2 when SiteMesh is removed.

--- web/tapestry/src/test/java/org/appfuse/webapp/pages/BasePageTestCase.java
+++ web/tapestry/src/test/java/org/appfuse/webapp/pages/BasePageTestCase.java
@@ -2,6 +2,7 @@ package org.appfuse.webapp.pages;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.tapestry5.TapestryFilter;
 import org.apache.tapestry5.dom.Document;
 import org.apache.tapestry5.internal.spring.SpringModuleDef;
 import org.apache.tapestry5.ioc.def.ModuleDef;
@@ -9,10 +10,17 @@ import org.apache.tapestry5.test.PageTester;
 import org.appfuse.Constants;
 import org.junit.After;
 import org.junit.Before;
+import org.springframework.beans.BeanUtils;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.context.ContextLoader;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.web.context.WebApplicationContext;
+import org.apache.tapestry5.spring.SpringConstants;
+import org.springframework.web.context.support.GenericWebApplicationContext;
 
+import javax.servlet.ServletContext;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,12 +40,26 @@ public abstract class BasePageTestCase extends AbstractTransactionalJUnit4Spring
     public void onSetUp() {
         String appPackage = "org.appfuse.webapp";
         String appName = "app";
+
+
+       final MockServletContext servletContext = new MockServletContext("");
+       // Workaround for "attribute is not of type WebApplicationContext" error  when using Spring 3.0.6.RELEASE
+       // T 5.3.2 will have Spring 3.1.0.RELEASE as dependency out-of-box
+       GenericWebApplicationContext wac = (GenericWebApplicationContext)
+               BeanUtils.instantiateClass(GenericWebApplicationContext.class);
+        wac.setParent(applicationContext);
+
+       servletContext.setAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE,
+               wac);
+        servletContext.addInitParameter(SpringConstants.USE_EXTERNAL_SPRING_CONTEXT, "true");
+
         tester = new PageTester(appPackage, appName, "src/main/webapp") {
             @Override
             protected ModuleDef[] provideExtraModuleDefs() {
-                return new ModuleDef[]{new SpringModuleDef(applicationContext)};
+                return new ModuleDef[]{new SpringModuleDef(servletContext)};
             }
         };
+
 
         fieldValues = new HashMap<String, String>();
 
@@ -49,6 +71,7 @@ public abstract class BasePageTestCase extends AbstractTransactionalJUnit4Spring
         mailSender.setPort(getSmtpPort());
         mailSender.setHost("localhost");
     }
+
 
     @After
     public void onTearDown() {
