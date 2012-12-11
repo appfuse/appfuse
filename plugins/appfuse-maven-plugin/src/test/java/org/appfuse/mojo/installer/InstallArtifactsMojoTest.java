63c9955d331ba38705ef4e278aa505b4e2330b98
Matt Raible <matt@raibledesigns.com>
2012-12-11 01:49:01 -0700

    Preparing for next development iteration

--- plugins/appfuse-maven-plugin/src/test/java/org/appfuse/mojo/installer/InstallArtifactsMojoTest.java
+++ plugins/appfuse-maven-plugin/src/test/java/org/appfuse/mojo/installer/InstallArtifactsMojoTest.java
@@ -5,7 +5,7 @@ import org.appfuse.mojo.AbstractAppFuseMojoTestCase;
 public class InstallArtifactsMojoTest extends AbstractAppFuseMojoTestCase {
 
     public void testCreateStrutsProject() throws Exception {
-        createTestProject("appfuse-basic-struts-archetype", "2.2");
+        createTestProject("appfuse-basic-struts-archetype", "2.3-SNAPSHOT");
         assertTrue("can't find test-project's pom.xml", checkExists("target/test-project/pom.xml"));
     }
 }
