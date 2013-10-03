be2673ed227619efaa709207d9300db26d8446c7
ivangsa <ivangsa@gmail.com>
2013-10-03 18:08:34 +0200

    Reformatting using spaces for indentation.

--- web/spring/src/test/java/org/appfuse/webapp/controller/UpdatePasswordControllerTest.java
+++ web/spring/src/test/java/org/appfuse/webapp/controller/UpdatePasswordControllerTest.java
@@ -16,12 +16,10 @@ import org.springframework.web.servlet.ModelAndView;
 import org.subethamail.wiser.Wiser;
 
 @ContextConfiguration(locations = {
-	"classpath:/applicationContext-resources.xml",
-	"classpath:/applicationContext-dao.xml",
-	"classpath:/applicationContext-service.xml",
-	"classpath*:/applicationContext.xml", // for modular archetypes
-	"/WEB-INF/applicationContext*.xml", "/WEB-INF/dispatcher-servlet.xml",
-	"/WEB-INF/security.xml" })
+ "classpath:/applicationContext-resources.xml",
+        "classpath:/applicationContext-dao.xml", "classpath:/applicationContext-service.xml",
+        "classpath*:/applicationContext.xml", // for modular archetypes
+        "/WEB-INF/applicationContext*.xml", "/WEB-INF/dispatcher-servlet.xml", "/WEB-INF/security.xml" })
 public class UpdatePasswordControllerTest extends BaseControllerTestCase {
     @Autowired
     private UpdatePasswordController controller;
@@ -31,144 +29,144 @@ public class UpdatePasswordControllerTest extends BaseControllerTestCase {
 
     @Test
     public void testRequestRecoveryToken() throws Exception {
-	String username = "admin";
-	MockHttpServletRequest request = newGet("/updatePassword");
-	request.addParameter("username", username);
+        String username = "admin";
+        MockHttpServletRequest request = newGet("/updatePassword");
+        request.addParameter("username", username);
 
-	// start SMTP Server
-	Wiser wiser = new Wiser();
-	wiser.setPort(getSmtpPort());
-	wiser.start();
+        // start SMTP Server
+        Wiser wiser = new Wiser();
+        wiser.setPort(getSmtpPort());
+        wiser.start();
 
-	controller.requestRecoveryToken(username, request);
+        controller.requestRecoveryToken(username, request);
 
-	// verify an account information e-mail was sent
-	wiser.stop();
-	assertTrue(wiser.getMessages().size() == 1);
+        // verify an account information e-mail was sent
+        wiser.stop();
+        assertTrue(wiser.getMessages().size() == 1);
 
-	// verify that success messages are in the session
-	assertNotNull(request.getSession().getAttribute(BaseFormController.MESSAGES_KEY));
+        // verify that success messages are in the session
+        assertNotNull(request.getSession().getAttribute(BaseFormController.MESSAGES_KEY));
     }
 
     @Test
     public void testShowUpdatePasswordForm() throws Exception {
-	String username = "admin";
-	MockHttpServletRequest request = newGet("/updatePassword");
-	request.addParameter("username", username);
+        String username = "admin";
+        MockHttpServletRequest request = newGet("/updatePassword");
+        request.addParameter("username", username);
 
-	ModelAndView mav = controller.showForm(username, null, request);
+        ModelAndView mav = controller.showForm(username, null, request);
 
-	assertEquals("updatePasswordForm", mav.getViewName());
+        assertEquals("updatePasswordForm", mav.getViewName());
 
     }
 
     @Test
     public void testShowResetPasswordForm() throws Exception {
-	String username = "admin";
-	User user = userManager.getUserByUsername(username);
-	String token = userManager.generateRecoveryToken(user);
+        String username = "admin";
+        User user = userManager.getUserByUsername(username);
+        String token = userManager.generateRecoveryToken(user);
 
-	MockHttpServletRequest request = newGet("/updatePassword");
-	request.addParameter("username", username);
-	request.addParameter("token", token);
+        MockHttpServletRequest request = newGet("/updatePassword");
+        request.addParameter("username", username);
+        request.addParameter("token", token);
 
-	ModelAndView mav = controller.showForm(username, token, request);
+        ModelAndView mav = controller.showForm(username, token, request);
 
-	assertEquals("updatePasswordForm", mav.getViewName());
-	assertNull(request.getSession().getAttribute(BaseFormController.ERRORS_MESSAGES_KEY));
+        assertEquals("updatePasswordForm", mav.getViewName());
+        assertNull(request.getSession().getAttribute(BaseFormController.ERRORS_MESSAGES_KEY));
     }
 
     @Test
     public void testShowResetPasswordFormBadToken() throws Exception {
-	String username = "admin";
-	String badtoken = RandomStringUtils.random(32);
+        String username = "admin";
+        String badtoken = RandomStringUtils.random(32);
 
-	MockHttpServletRequest request = newGet("/updatePassword");
-	request.addParameter("username", username);
-	request.addParameter("token", badtoken);
+        MockHttpServletRequest request = newGet("/updatePassword");
+        request.addParameter("username", username);
+        request.addParameter("token", badtoken);
 
-	ModelAndView mav = controller.showForm(username, badtoken, request);
+        ModelAndView mav = controller.showForm(username, badtoken, request);
 
-	assertNotNull(request.getSession().getAttribute(BaseFormController.ERRORS_MESSAGES_KEY));
+        assertNotNull(request.getSession().getAttribute(BaseFormController.ERRORS_MESSAGES_KEY));
     }
 
     @Test
     public void testResetPassword() throws Exception {
-	String username = "admin";
-	User user = userManager.getUserByUsername(username);
-	String token = userManager.generateRecoveryToken(user);
-	String password = "new-pass";
+        String username = "admin";
+        User user = userManager.getUserByUsername(username);
+        String token = userManager.generateRecoveryToken(user);
+        String password = "new-pass";
 
-	MockHttpServletRequest request = newGet("/updatePassword");
-	request.setRemoteUser(username);
-	request.addParameter("username", username);
-	request.addParameter("token", token);
-	request.addParameter("password", password);
+        MockHttpServletRequest request = newGet("/updatePassword");
+        request.setRemoteUser(username);
+        request.addParameter("username", username);
+        request.addParameter("token", token);
+        request.addParameter("password", password);
 
-	Wiser wiser = new Wiser();
-	wiser.setPort(getSmtpPort());
-	wiser.start();
+        Wiser wiser = new Wiser();
+        wiser.setPort(getSmtpPort());
+        wiser.start();
 
-	ModelAndView mav = controller.onSubmit(username, token, null, password, request);
+        ModelAndView mav = controller.onSubmit(username, token, null, password, request);
 
-	wiser.stop();
-	assertTrue(wiser.getMessages().size() == 1);
+        wiser.stop();
+        assertTrue(wiser.getMessages().size() == 1);
 
-	assertNotNull(request.getSession().getAttribute(BaseFormController.MESSAGES_KEY));
-	assertNull(request.getSession().getAttribute(BaseFormController.ERRORS_MESSAGES_KEY));
+        assertNotNull(request.getSession().getAttribute(BaseFormController.MESSAGES_KEY));
+        assertNull(request.getSession().getAttribute(BaseFormController.ERRORS_MESSAGES_KEY));
     }
-    
+
     @Test
     public void testResetPasswordBadToken() throws Exception {
-	String username = "admin";
-	String badToken = RandomStringUtils.random(32);
-	String password = "new-pass";
+        String username = "admin";
+        String badToken = RandomStringUtils.random(32);
+        String password = "new-pass";
 
-	MockHttpServletRequest request = newGet("/updatePassword");
-	request.setRemoteUser(username);
-	request.addParameter("username", username);
-	request.addParameter("token", badToken);
-	request.addParameter("password", password);
+        MockHttpServletRequest request = newGet("/updatePassword");
+        request.setRemoteUser(username);
+        request.addParameter("username", username);
+        request.addParameter("token", badToken);
+        request.addParameter("password", password);
 
-	ModelAndView mav = controller.onSubmit(username, badToken, null, password, request);
+        ModelAndView mav = controller.onSubmit(username, badToken, null, password, request);
 
-	assertNull(request.getSession().getAttribute(BaseFormController.MESSAGES_KEY));
-	assertNotNull(request.getSession().getAttribute(BaseFormController.ERRORS_MESSAGES_KEY));
+        assertNull(request.getSession().getAttribute(BaseFormController.MESSAGES_KEY));
+        assertNotNull(request.getSession().getAttribute(BaseFormController.ERRORS_MESSAGES_KEY));
     }
-    
+
     @Test
     public void testUpdatePassword() throws Exception {
-	String username = "admin";
-	String currentPassword = "admin";
-	String password = "new-pass";
+        String username = "admin";
+        String currentPassword = "admin";
+        String password = "new-pass";
 
-	MockHttpServletRequest request = newGet("/updatePassword");
-	request.setRemoteUser(username);
-	request.addParameter("username", username);
-	request.addParameter("currentPassword", currentPassword);
-	request.addParameter("password", password);
+        MockHttpServletRequest request = newGet("/updatePassword");
+        request.setRemoteUser(username);
+        request.addParameter("username", username);
+        request.addParameter("currentPassword", currentPassword);
+        request.addParameter("password", password);
 
-	ModelAndView mav = controller.onSubmit(username, null, currentPassword, password, request);
+        ModelAndView mav = controller.onSubmit(username, null, currentPassword, password, request);
 
-	assertNotNull(request.getSession().getAttribute(BaseFormController.MESSAGES_KEY));
-	assertNull(request.getSession().getAttribute(BaseFormController.ERRORS_MESSAGES_KEY));
+        assertNotNull(request.getSession().getAttribute(BaseFormController.MESSAGES_KEY));
+        assertNull(request.getSession().getAttribute(BaseFormController.ERRORS_MESSAGES_KEY));
     }
-    
+
     @Test
     public void testUpdatePasswordBadCurrentPassword() throws Exception {
-	String username = "admin";
-	String currentPassword = "bad";
-	String password = "new-pass";
+        String username = "admin";
+        String currentPassword = "bad";
+        String password = "new-pass";
 
-	MockHttpServletRequest request = newGet("/updatePassword");
-	request.setRemoteUser(username);
-	request.addParameter("username", username);
-	request.addParameter("currentPassword", currentPassword);
-	request.addParameter("password", password);
+        MockHttpServletRequest request = newGet("/updatePassword");
+        request.setRemoteUser(username);
+        request.addParameter("username", username);
+        request.addParameter("currentPassword", currentPassword);
+        request.addParameter("password", password);
 
-	ModelAndView mav = controller.onSubmit(username, null, currentPassword, password, request);
+        ModelAndView mav = controller.onSubmit(username, null, currentPassword, password, request);
 
-	assertNull(request.getSession().getAttribute(BaseFormController.MESSAGES_KEY));
-	assertNotNull(request.getSession().getAttribute(BaseFormController.ERRORS_MESSAGES_KEY));
+        assertNull(request.getSession().getAttribute(BaseFormController.MESSAGES_KEY));
+        assertNotNull(request.getSession().getAttribute(BaseFormController.ERRORS_MESSAGES_KEY));
     }
 }
