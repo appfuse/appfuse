e6157cf6dcdc46521b8b1d640678a2ec901462a2
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2013-12-11 23:30:56 +0100

    WIP: review pom dependencies and versiones from upstream

---
+++ script.sh
@@ -0,0 +1,7 @@
+rm -rf /tmp/appfuse-gwt
+mkdir /tmp/appfuse-gwt
+git archive master | tar -x -C /tmp/appfuse-gwt
+gedit /tmp/appfuse-gwt/pom.xml /tmp/appfuse-gwt/web/pom.xml /tmp/appfuse-gwt/archetypes/pom.xml /tmp/appfuse-gwt/archetypes/build.xml
+cd /tmp/appfuse-gwt
+mvn clean install -P gwtDebug,itest -DaltDeploymentRepository=local-repo::default::file:/home/opt/workspaces/AppFuse/repo-2.2.2
+
