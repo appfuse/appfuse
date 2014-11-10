b341f7f7fdb5d91eae2ae3e9867b77811dee28b8
Matt Raible <matt@raibledesigns.com>
2014-11-10 15:02:00 -0700

    APF-1451: Refactored to use Spring MVC Test.

--- web/spring/src/test/java/org/appfuse/webapp/controller/PasswordHintControllerTest.java
+++ web/spring/src/test/java/org/appfuse/webapp/controller/PasswordHintControllerTest.java
@@ -1,33 +1,51 @@
 package org.appfuse.webapp.controller;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.subethamail.wiser.Wiser;
 
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class PasswordHintControllerTest extends BaseControllerTestCase {
     @Autowired
-    private PasswordHintController c = null;
+    private PasswordHintController controller;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
 
     @Test
     public void testExecute() throws Exception {
-        MockHttpServletRequest request = newGet("/passwordHint.html");
-        request.addParameter("username", "user");
-
        // start SMTP Server
         Wiser wiser = new Wiser();
         wiser.setPort(getSmtpPort());
         wiser.start();
-        
-        c.handleRequest(request);
-        
+
+        ResultActions actions = mockMvc.perform(get("/passwordHint.html").param("username", "user"))
+            .andExpect(status().is3xxRedirection());
+
+        MvcResult result = actions.andReturn();
+        MockHttpSession session = (MockHttpSession) result.getRequest().getSession();
+        // verify that success messages are in the session
+        assertNotNull(session.getAttribute(BaseFormController.MESSAGES_KEY));
+
         // verify an account information e-mail was sent
         wiser.stop();
         assertTrue(wiser.getMessages().size() == 1);
-        
-        // verify that success messages are in the session
-        assertNotNull(request.getSession().getAttribute(BaseFormController.MESSAGES_KEY));
     }
 }
