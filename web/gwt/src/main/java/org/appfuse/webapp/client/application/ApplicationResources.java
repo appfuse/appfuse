d96e186569bce0ec4bf9906d46d38c3a2a8911ea
Matt Raible <matt@raibledesigns.com>
2015-02-19 18:42:32 -0700

    Preparing for Release 3.5.0

--- web/gwt/src/main/java/org/appfuse/webapp/client/application/ApplicationResources.java
+++ web/gwt/src/main/java/org/appfuse/webapp/client/application/ApplicationResources.java
@@ -1503,11 +1503,11 @@ public interface ApplicationResources extends com.google.gwt.i18n.client.Message
     String webapp_tagline();
 
     /**
-     * Translated "Version 3.5.0-SNAPSHOT".
+     * Translated "Version 3.5.0".
      *
-     * @return translated "Version 3.5.0-SNAPSHOT"
+     * @return translated "Version 3.5.0"
      */
-    @DefaultMessage("Version 3.5.0-SNAPSHOT")
+    @DefaultMessage("Version 3.5.0")
     @Key("webapp.version")
     String webapp_version();
 }
